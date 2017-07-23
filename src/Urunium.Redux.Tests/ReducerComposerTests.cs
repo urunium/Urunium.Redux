4c43b6b7d1f8dcfa6e9507042210cb3fe8cb82f9
nripendra <nripendra.newa@gmail.com>
2017-07-23 21:20:23 +0545

    Composer class for reducer. This will serve as helper to be used as root reducer

---
+++ src/Urunium.Redux.Tests/ReducerComposerTests.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Urunium.Redux.Compose;
+
+namespace Urunium.Redux.Tests
+{
+    [TestFixture]
+    public class ReducerComposerTests
+    {
+        [Test]
+        public void ApplyShowForwardCallToRegisteredReducer()
+        {
+            Mock<IReducer<TestState>> mockIReducer = new Mock<IReducer<TestState>>();
+            mockIReducer.Setup(x => x.Apply(It.IsAny<TestState>(), It.IsAny<object>())).Returns(new TestState());
+            ReducerComposer<TestState> composer = new ReducerComposer<TestState>();
+            composer.AddStateReducer(mockIReducer.Object);
+            var result = composer.Apply(new TestState(), "");
+            mockIReducer.Verify(x => x.Apply(It.IsAny<TestState>(), It.IsAny<object>()));
+        }
+
+        [Test]
+        public void CanBeUsedAsRootReducer()
+        {
+            Mock<IReducer<TestState>> mockIReducer = new Mock<IReducer<TestState>>();
+            mockIReducer.Setup(x => x.Apply(It.IsAny<TestState>(), It.IsAny<object>())).Returns(new TestState());
+            ReducerComposer<TestState> composer = new ReducerComposer<TestState>();
+            composer.AddStateReducer(mockIReducer.Object);
+
+            Store<TestState> store = new Store<TestState>(composer, new TestState());
+            store.Dispatch("");
+            mockIReducer.Verify(x => x.Apply(It.IsAny<TestState>(), It.IsAny<object>()));
+        }
+
+        public class TestState
+        {
+
+        }
+    }
+}
---
+++ src/Urunium.Redux/Compose/ReducerComposer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urunium.Redux.Compose
+{
+    /// <summary>
+    /// Root reducer that will compose various reducers.
+    /// </summary>
+    /// <typeparam name="TState">Type of state</typeparam>
+    public class ReducerComposer<TState> : IReducer<TState>
+    {
+        private List<IReducer<TState>> _reducers = new List<IReducer<TState>>();
+
+        /// <summary>
+        /// Add a reducer.
+        /// </summary>
+        /// <param name="stateReducer"></param>
+        /// <returns></returns>
+        public ReducerComposer<TState> AddStateReducer(IReducer<TState> stateReducer)
+        {
+            _reducers.Add(stateReducer);
+            return this;
+        }
+
+        /// <summary>
+        /// Apply action to state using registered reducers.
+        /// </summary>
+        /// <param name="previousState">State that needs to be transformed</param>
+        /// <param name="action">Action that needs to be applied to state.</param>
+        /// <returns>Resulting state after applying action.</returns>
+        public TState Apply(TState previousState, object action)
+        {
+            var nextState = previousState;
+            foreach(var reducer in _reducers)
+            {
+                nextState = reducer.Apply(nextState, action);
+            }
+            return nextState;
+        }
+    }
+}
