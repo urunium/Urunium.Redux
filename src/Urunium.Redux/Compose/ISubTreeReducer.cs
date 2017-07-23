ee5be4baa92d67aecc58e32c35ad55c53e2685ba
nripendra <nripendra.newa@gmail.com>
2017-07-23 21:28:18 +0545

    Define sub-tree reducer

---
+++ src/Urunium.Redux/Compose/ISubTreeReducer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urunium.Redux.Compose
+{
+    /// <summary>
+    /// Reducer that works on a part (sub-tree) of application state.
+    /// Hence helps in dividing reducers into small reducers, processing independently
+    /// smaller part of the state tree.
+    /// </summary>
+    /// <typeparam name="TState">Type of full state</typeparam>
+    /// <typeparam name="TPart">Type of property which this reducer will deal with.</typeparam>
+    public interface ISubTreeReducer<TState, TPart> : IReducer<TPart>
+    {
+        System.Linq.Expressions.Expression<Func<TState, TPart>> PropertySelector { get; }
+    }
+}
