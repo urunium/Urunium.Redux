1e6be213af4978cb44d0cabad3cf300fb1cb959c
nripendra <nripendra.newa@gmail.com>
2017-07-23 17:19:57 +0545

    Define store and reducer.

---
+++ src/Urunium.Redux/IReducerOfT.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urunium.Redux
+{
+    /// <summary>
+    /// Reducer takes in a state, applies an action and returns resultant state.
+    /// Action can be any object that is supported by reducer, there isn't any hard
+    /// and fast rule for action. Could even be primitive types.
+    /// </summary>
+    /// <typeparam name="TState">Type of state</typeparam>
+    public interface IReducer<TState>
+    {
+        /// <summary>
+        /// Apply an action to old state and return a new state.
+        /// </summary>
+        /// <param name="previousState">Existing state</param>
+        /// <param name="action">Action that needs to be applied</param>
+        /// <returns>New state</returns>
+        TState Apply(TState previousState, object action);
+    }
+}
---
+++ src/Urunium.Redux/IStore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urunium.Redux
+{
+    /// <summary>
+    /// Store is responsible for holding state. Dispatch action to reducer, 
+    /// and notify about change in state to subscribers.
+    /// </summary>
+    /// <typeparam name="TState"></typeparam>
+    public interface IStore<TState>
+    {
+        /// <summary>
+        /// Application's State object which needs to be held by this store.
+        /// </summary>
+        TState State { get; }
+
+        /// <summary>
+        /// Dispatch action to reducers which will then apply the actions.
+        /// Also, notifies about state change by firing StageChanged event.
+        /// </summary>
+        /// <typeparam name="TAction">Type of action</typeparam>
+        /// <param name="action">Action Object</param>
+        void Dispatch<TAction>(TAction action);
+
+        /// <summary>
+        /// Can be subscribed to get notified about change in state.
+        /// </summary>
+        event EventHandler<EventArgs> StateChanged;
+    }
+}
