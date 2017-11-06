2f1a3abf3ca18bd6f03d72b80c48d66757fe4c81
nripendra <nripendra.newa@gmail.com>
2017-11-06 15:38:39 +0545

    Remove real-example from main solution.

--- src/UruniumWPFExample/Actions/CompleteAllTodos.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UruniumWPFExample.Actions
-{
-    /// <summary>
-    /// Action to mark all todo items as completed.
-    /// </summary>
-    public class CompleteAllTodos
-    {
-    }
-}
